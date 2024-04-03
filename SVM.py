3040531ec32832e6d8b2faedf49bd2a6d67fa1f7
Ashley11333 <ashlao@163.com>
2024-04-03 14:05:00 +1300

    svm

--- SVM.py
+++ SVM.py
@@ -1,21 +1,56 @@
 import numpy as np
-import os
-from pathlib import Path
 import pandas as pd
-import matplotlib.pyplot as plt
-from keras.preprocessing import image
+import matplotlib.image as mpimg
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from pathlib import Path
 
-# load dataset and get images name and labels
-script_dir = os.path.dirname(os.path.abspath(__file__))
-tomatoDatasetPathTrain = os.path.join(script_dir, "Tomato pest-diseases.v1-tomato_v1.multiclass/train/_classes.csv")
-df = pd.read_csv(tomatoDatasetPathTrain)
-tomatoImagesName = df["filename"]
 
-# get labels for each image
-labels = df.drop('filename', axis=1)
-image_labels = labels.to_dict(orient='records')
+script_dir = Path(__file__).parent  
+train_dir = script_dir / "train"
+test_dir = script_dir / "test"
+
+
+df_train = pd.read_csv(train_dir / "_classes.csv")
+df_test = pd.read_csv(test_dir / "_classes.csv")
 
 imageDataTrain = []
-for i in tomatoDatasetPathTrain.glob("*.jpg"):
-    img = image.load_img(i, target_size=(32,32))
-    print(img)
+imageDataTest = []
+
+labels_train = df_train.iloc[:, 1:].values  
+
+labels_test = df_test.iloc[:, 1:].values
+
+
+for filename in df_train["filename"]:
+    img_path = train_dir / filename
+    if img_path.is_file():
+        img_array = mpimg.imread(img_path)
+        img_array = np.resize(img_array, (8, 8, 3))
+        img_array = img_array.flatten() / 255.0
+        imageDataTrain.append(img_array)
+
+
+for filename in df_test["filename"]:
+    img_path = test_dir / filename
+    if img_path.is_file():
+        img_array = mpimg.imread(img_path)
+        img_array = np.resize(img_array, (8, 8, 3))
+        img_array = img_array.flatten() / 255.0
+        imageDataTest.append(img_array)
+
+imageDataTrain = np.array(imageDataTrain)
+imageDataTest = np.array(imageDataTest)
+
+svm_model = OneVsRestClassifier(SVC(kernel='linear', probability=True))
+svm_model.fit(imageDataTrain, labels_train)
+
+predictions = svm_model.predict(imageDataTest)
+
+accuracies = []
+for i in range(labels_test.shape[1]):  
+    accuracies.append(accuracy_score(labels_test[:, i], predictions[:, i]))
+average_accuracy = np.mean(accuracies)
+
+print(f"Average model accuracy across all labels: {average_accuracy}")
